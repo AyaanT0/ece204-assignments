1ec796fed373729f5db87a213f74525b18f0ad3b
Ayaan Tunio <ayaantunio1@gmail.com>
2026-02-28 11:25:48 -0500

    update to use stdexcept, add test cases from crowdmark

--- project 3/main.cpp
+++ project 3/main.cpp
@@ -74,13 +74,105 @@ int main() {
   // Real roots:
   //     +3
 
+
+
+
+  // Question 1
+  /*
+  double p0a[1]{ 0.7 };
+  unsigned int d0a{ roots( p0a, 0 ) };
+  print( p0a, d0a, 0 );
+
+  double p0b[1]{ 0.0 };
+  unsigned int d0b{ roots( p0b, 0 ) };
+  print( p0b, d0b, 0 );
+  */
+
+
+  // Question 2
+  /*
+  double p1a[2]{ 0.7, 0.5 };
+  unsigned int d1a{ roots( p1a, 1 ) };
+  print( p1a, d1a, 1 );
+
+  double p1b[2]{ 0.7, -0.5 };
+  unsigned int d1b{ roots( p1b, 1 ) };
+  print( p1b, d1b, 1 );
+  */
+
+
+  // Question 3
+  /*
+  double p2a[3]{ 0.3, 0.1, 0.5 };
+  unsigned int d2a{ roots( p2a, 2 ) };
+  print( p2a, d2a, 2 );
+
+  double p2b[3]{ 0.7, -0.5, -0.2 };
+  unsigned int d2b{ roots( p2b, 2 ) };
+  print( p2b, d2b, 2 );
+  */
+
+
+  // Question 4
+  /*
+  double p3a[4]{ -0.2, 0.3, -0.9, 0.3 };
+  unsigned int d3a{ roots( p3a, 3 ) };
+  print( p3a, d3a, 3 );
+
+  double p3b[4]{ 0.1, 0.3, -0.9, 0.3 };
+  unsigned int d3b{ roots( p3b, 3 ) };
+  print( p3b, d3b, 3 );
+  */
+
+
+  // Question 5
+  /*
+  double p4a[5]{ -12.0, -8.4, 2.4, 0.0, 1.2 };
+  unsigned int d4a{ roots( p4a, 4 ) };
+  print( p4a, d4a, 4 );
+
+  double p4b[5]{ -14.4, -19.2, -1.2, 4.8, 1.2 };
+  unsigned int d4b{ roots( p4b, 4 ) };
+  print( p4b, d4b, 4 );
+
+  double p4c[5]{ -54.0, -37.8, -29.7, -8.1, -2.7 };
+  unsigned int d4c{ roots( p4c, 4 ) };
+  print( p4c, d4c, 4 );
+  */
+
+
+  // Question 6
+  /*
+  double p5a[6]{ -157.50, 68.25, 29.75, 12.25, -5.25, -3.5 };
+  unsigned int d5a{ roots( p5a, 5 ) };
+  print( p5a, d5a, 5 );
+
+  double p5b[6]{ -16.2, 47.25, -22.275, -19.575, 8.1, 2.7 };
+  unsigned int d5b{ roots( p5b, 5 ) };
+  print( p5b, d5b, 5 );
+
+  double p5c[6]{
+    -739.3056852825, 379.9560444975, -216.18149622,
+      63.3701740,    -19.2249,          4.1
+  };
+  unsigned int d5c{ roots( p5c, 5 ) };
+  print( p5c, d5c, 5 );
+  */
+
+
+  // Question 7
+  /*
+  double p7[3]{ 0.7, -0.5, 0.0 };
+  unsigned int d7{ roots( p7, 2 ) };
+  print( p7, d7, 2 );
+  */
   return 0;
 }
 
 // Find all real roots of polynomial
 unsigned int roots( double coeffs[], unsigned int degree ) {
   if (degree == 0) return 0; // Const has no roots
-  if (coeffs[degree] == 0.0) return NAN; // Leading coeff must be nonzero
+  if (coeffs[degree] == 0.0) throw std::invalid_argument("leading coeff can't be zero"); // Leading coeff must be nonzero
 
   unsigned int deg = degree;
 
@@ -100,17 +192,17 @@ unsigned int roots( double coeffs[], unsigned int degree ) {
 // Newton's method to approx a root
 double newton( double coeffs[], unsigned int degree ) {
   if (degree == 0) return NAN; // Const has no roots
-  if (coeffs[degree] == 0.0) return NAN; // Leading coeff must be nonzero
+  if (coeffs[degree] == 0.0) throw std::invalid_argument("leading coeff can't be zero"); // Leading coeff must be nonzero
 
   double init_guess = 211.19343; // Student number is 21119343 (arbitrary initial guess)
 
   // Iterate to find convergence and determine root
   for (int i = 0; i < 1000; i++) { // Max iterations arbitrarily set to 1000
     double f = horner(init_guess, coeffs, degree);
-    double df = dhorner(init_guess, coeffs, degree);
-
-    if (std::abs(df) < 1e-9) return NAN; // If derivative is (close to) zero then Newton's method will not work
     if (std::abs(f) < 1e-9) return init_guess; // If (close to) zero, found root
+
+    double df = dhorner(init_guess, coeffs, degree);
+    if (std::abs(df) < 1e-9) return NAN; // If derivative is (close to) zero then Newton's method will not work
 
     double new_guess = init_guess - f / df; // Update the guess (newtons method formula)
     if (std::abs(new_guess - init_guess) < 1e-9) return new_guess; // If step size is super close to zero, converged
